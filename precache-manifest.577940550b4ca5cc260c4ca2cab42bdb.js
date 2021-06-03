1726da05d71f644fbf9f9c0ee170813273840983
Rikard <rille_anderson@hotmail.com>
2021-06-03 10:54:13 +0200

    Updates

---
+++ precache-manifest.577940550b4ca5cc260c4ca2cab42bdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c521cde36a998c3090548893a61a12",
+    "url": "/%7BMyResume%7D/index.html"
+  },
+  {
+    "revision": "e9b0a6960b26a71ff26f",
+    "url": "/%7BMyResume%7D/static/css/2.56e33495.chunk.css"
+  },
+  {
+    "revision": "9e4f845193233c06c974",
+    "url": "/%7BMyResume%7D/static/css/main.51180617.chunk.css"
+  },
+  {
+    "revision": "e9b0a6960b26a71ff26f",
+    "url": "/%7BMyResume%7D/static/js/2.0b85b5dd.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/%7BMyResume%7D/static/js/2.0b85b5dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4f845193233c06c974",
+    "url": "/%7BMyResume%7D/static/js/main.537a97c3.chunk.js"
+  },
+  {
+    "revision": "0fc25973c8040425cffb",
+    "url": "/%7BMyResume%7D/static/js/runtime-main.f1c9d687.js"
+  },
+  {
+    "revision": "0e18c132e9ebe60210501b0b8ad7c06b",
+    "url": "/%7BMyResume%7D/static/media/Me.0e18c132.jpg"
+  }
+]);
